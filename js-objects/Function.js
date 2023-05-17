7a08772ae8a3f67df1718a525e633fc0f5cbf7a4
Ferreira Josué <ferreira.josu@gmail.com>
2023-05-17 19:16:12 +0200

    created velocity function

--- js-objects/Function.js
+++ js-objects/Function.js
@@ -8,12 +8,12 @@ const invaderAttack = (allInvaders, life, refreshIntervalId1, refreshIntervalId2
         allInvaders.forEach(invader => {
             canGoDown = invader.goDown();
             shottedShuttle = invader.isShotted(document.getElementById('shuttle').getBoundingClientRect());
-            if(shottedShuttle && life > 0){
-                life--;
+            if(shottedShuttle && life.value > 0){
+                life.value--;
                 document.getElementById('shuttle').classList.add('loselife');
                 setTimeout(() => document.getElementById('shuttle').classList.remove('loselife'), 200);
             }
-            else if(life == 0){
+            else if(life.value == 0){
                 clearInterval(refreshIntervalId1);
                 clearInterval(refreshIntervalId2);
                 clearInterval(refreshIntervalId3);
@@ -24,7 +24,44 @@ const invaderAttack = (allInvaders, life, refreshIntervalId1, refreshIntervalId2
                 allInvaders.shift();
         });   
     }
-    return life;
 }
 
-export {invaderAttack};
+const setVelocity = (velocity, score, timeInvaderAppear, timeInvaderDown, refreshIntervalId1, refreshIntervalId2, refreshIntervalId3, allInvaders, life) => {
+    switch(velocity){
+        case 1: if(score > 5){
+                    timeInvaderAppear = 1500;
+                    timeInvaderDown = 400;
+                    console.log(timeInvaderAppear)
+                    console.log(timeInvaderDown)
+                    clearInterval(refreshIntervalId2);
+                    refreshIntervalId2 = setInterval(() => {
+                        allInvaders.push(new Invader());
+                    },timeInvaderAppear);
+                    clearInterval(refreshIntervalId3);
+                    refreshIntervalId3 = setInterval(() => {
+                        invaderAttack(allInvaders,life,refreshIntervalId1,refreshIntervalId2,refreshIntervalId3);
+                    },timeInvaderDown);
+                    velocity++;
+                }
+                break;
+        case 2: if(score > 10){
+                timeInvaderAppear = 1000;
+                timeInvaderDown = 300;
+                console.log(timeInvaderAppear)
+                console.log(timeInvaderDown)
+                clearInterval(refreshIntervalId2);
+                refreshIntervalId2 = setInterval(() => {
+                    allInvaders.push(new Invader());
+                },timeInvaderAppear);
+                clearInterval(refreshIntervalId3);
+                refreshIntervalId3 = setInterval(() => {
+                    invaderAttack(allInvaders,life,refreshIntervalId1,refreshIntervalId2,refreshIntervalId3);
+                },timeInvaderDown);
+                velocity++;
+            }
+            break;
+    }
+    return velocity;
+}
+
+export {invaderAttack, setVelocity};
